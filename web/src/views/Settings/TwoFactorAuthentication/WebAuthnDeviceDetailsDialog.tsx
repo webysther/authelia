bcfb7559dbe745ed0e39a1b71e47bd918ee09308
James Elliott <james-d-elliott@users.noreply.github.com>
2023-04-22 14:19:57 +1000

    refactor: simplified
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

--- web/src/views/Settings/TwoFactorAuthentication/WebAuthnDeviceDetailsDialog.tsx
+++ web/src/views/Settings/TwoFactorAuthentication/WebAuthnDeviceDetailsDialog.tsx
@@ -8,6 +8,8 @@ import {
     DialogContentText,
     DialogTitle,
     Divider,
+    Paper,
+    PaperProps,
     Typography,
 } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
@@ -27,7 +29,12 @@ const WebAuthnDeviceDetailsDialog = function (props: Props) {
     const { t: translate } = useTranslation("settings");
 
     return (
-        <Dialog open={props.open} onClose={props.handleClose} aria-labelledby="webauthn-device-details-dialog-title">
+        <Dialog
+            open={props.open}
+            onClose={props.handleClose}
+            PaperComponent={PaperComponent}
+            aria-labelledby="webauthn-device-details-dialog-title"
+        >
             <DialogTitle id="webauthn-device-details-dialog-title">
                 {translate("WebAuthn Credential Details")}
             </DialogTitle>
@@ -161,6 +168,14 @@ function PropertyText(props: PropertyTextProps) {
             </Typography>
             <Typography display="inline">{props.value}</Typography>
         </Grid>
+    );
+}
+
+function PaperComponent(props: PaperProps) {
+    return (
+        <Draggable handle="#webauthn-device-details-dialog-title" cancel={'[class*="MuiDialogContent-root"]'}>
+            <Paper {...props} />
+        </Draggable>
     );
 }
 
